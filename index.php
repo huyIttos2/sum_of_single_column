237d28a67fd65a1ece75603c8bfe948d49092de0
huyIttos2 <huyitto071195@gmail.com>
2019-08-05 14:22:43 +0700

    sum_of_a_column

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+//Initialize matrix a
+$a = array(
+    array(1, 2, 3),
+    array(4, 5, 6),
+    array(7, 8, 9)
+);
+
+$rows = count($a);
+$cols = count($a[0]);
+
+for($i = 0; $i < $rows; $i++){
+    $sumRow = 0;
+    for($j = 0; $j < $cols; $j++){
+        $sumRow = $sumRow + $a[$i][$j];
+    }
+    print("Sum of " . ($i+1) ." row: " . $sumRow);
+    print("<br>");
+}
+
+for($i = 0; $i < $cols; $i++){
+    $sumCol = 0;
+    for($j = 0; $j < $rows; $j++){
+        $sumCol = $sumCol + $a[$j][$i];
+    }
+    print("Sum of " . ($i+1) . " column: " . $sumCol);
+    print("<br>");
+}
+?>
+</body>
+</html>
